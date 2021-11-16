75e4604ec4b31f117a6e030bce2a08453e2b4ec1
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2021-11-16 09:46:05 +0100

    commit

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,4 @@
-#maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
-#elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
-#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+def game_etc ():
+ print("dans")
+ print("maak muziek")
+ print("voetballen")
