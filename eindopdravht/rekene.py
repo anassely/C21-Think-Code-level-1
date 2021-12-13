75717ec51d503c22d01f6baa49dea6285ee7f70e
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2021-12-13 12:33:10 +0100

    verander

--- eindopdravht/rekene.py
+++ eindopdravht/rekene.py
@@ -1,19 +1,35 @@
-A= input()
+print("hello, wat is jouw naam")
+
+print("hallo, wat is jouw naam")
+name = input ("type je naam ")
+
+print("Welkom "+ name )
+start = input("wil je een quiz maken " + name + " ") 
+
+if start =="ja":
+    print("laten we beginnen! ")
+
+    eerste_vraag = input ("vind je deze opleiding leuk? ")
+        
+    if eerste_vraag =="ja":
+        print("ik vind het ook leuk! ")
+    else:
+        print("verander dan van opleiding. ")
+
+    tweede_vraag = input ("hou je van cola? ")
+        
+    if tweede_vraag =="ja":
+        print("ik vind ook! ")
+    else:
+        print("oh jammer ")
+
+    derde_vraag = input ("ben je gelukkig? ")
+        
+    if derde_vraag =="ja":
+        print("ik ook ")
+    else:
+        print("jammer ")
 
 
-
-print(A*35 )
-
-
-
-ifA == ("monke"):
-
-print("hahaha funny monke")
-
-else :
-
-print("say monke")
-
-
-
-print("dat zijn veel",A)
+else:
+    print("jammer ")
